5927d831900c3d4e504090e9e7211eb88e343673
oleksiizadorozhnyi <91991560+oleksiizadorozhnyi@users.noreply.github.com>
2023-05-28 02:04:37 +0300

    addCtx

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"context"
 	"perviymoiserver/pkg/adapter/rest"
 	"perviymoiserver/pkg/adapter/storage/mongo"
 	"perviymoiserver/pkg/shortener"
 )
 
 func main() {
-	storage := mongo.New()
+	ctx := context.Background()
+	storage := mongo.New(ctx)
 	s := shortener.New(storage)
 	server := rest.New(s)
 	err := server.Start()
